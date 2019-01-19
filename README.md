cdadf1bec88c179d2a86c9c215f4e6c027b0112a
hiroshi fujita <fuchantoshu@gmail.com>
2019-01-19 14:48:48 +0900

    add: front/archive pages

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # PORTFOLIO_THEME
 
 PORTFOLIO_THEME Repository.
+
+## File/Directory Architecture
+
+``` text
+.
+├── LICENSE
+├── README.md
+├── server
+│   ├── cms
+│   ├── db
+│   └── docker-compose.yml
+└── themes
+    ├── gulpfile.babel.js
+    ├── package-lock.json
+    ├── package.json
+    ├── portfolio
+    └── sass
+```
--- themes/portfolio/archive.php
+++ themes/portfolio/archive.php
@@ -1,9 +1,9 @@
 <?php
   get_header();
 ?>
-<div class="content--archives">
-  <h1 class="title content__title">Archives : <?php single_cat_title(); ?></h1>
-  <div class="list list--archives">
+<div class="content-archive">
+  <h1 class="title title--category"><?php single_cat_title('', true); ?></h1>
+  <div class="list">
 <?php
   if (have_posts()) {
     while (have_posts()) {
