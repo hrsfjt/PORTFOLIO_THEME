1e262f78799f7bf920843ad5cc5534f23f4629f2
hiroshi fujita <fuchantoshu@gmail.com>
2019-05-15 07:48:40 +0900

    update: 基本レイアウトスタイル

--- themes/portfolio/single.php
+++ themes/portfolio/single.php
@@ -3,7 +3,6 @@
   if (have_posts()) {
     while (have_posts()) {
       the_post();
-      $image_url = get_the_post_thumbnail_url($post->ID, 'full');
 ?>
 <article class="article">
   <header class="article__header">
