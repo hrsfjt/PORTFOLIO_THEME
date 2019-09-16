330b5526074e03b7f706d9d662e3d044f2e014ea
hiroshi fujita <fuchantoshu@gmail.com>
2019-09-16 14:45:40 +0900

    update: uploadスクリプト

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-if [ ! -e .env ]; then
-  echo "required .env file."
-  exit
-fi
 
-. .env
+check_env() {
+  if [ ! -e .env ]; then
+    echo "required .env file."
+    exit
+  fi
+  . .env
+}
+
+check_version() {
+  if [ ! -e ".version" ]; then
+    touch .version
+    /bin/echo -n "1.0.0" >.version
+    /bin/echo "check .version file"
+    exit
+  fi
+}
+
+check_deploylog() {
+  if [ ! -e "deploy.log" ]; then
+    touch deploy.log
+  fi
+}
